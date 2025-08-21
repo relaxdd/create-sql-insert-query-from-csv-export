43def789df810ddc950271e345d6479982006b55
relaxdd <awenn2015@gmail.com>
2025-08-22 02:19:44 +0400

    Update dependencies

--- src/example.ts
+++ src/example.ts
@@ -1,30 +1,39 @@
 import createSqlInsertQueryFromCsvExport from './index'
 
-// declare var increment: number
-// declare var csvDataChunks: string[]
+declare var increment: number
+declare var csvDataChunks: string[]
 
-// ;((csvData) => {
-//   increment++
-
-//   return createSqlInsertQueryFromCsvExport({
-//     csvData,
-//     tableName: 'public.wshd_order_incidents',
-//     tableColumns: ['code', 'time', 'text', 'avr_code', 'avr_text', 'orders_in_work_shift_data_id'],
-//     removeHeader: true,
-//     skipRowIfNullableCols: { code: true },
-//     replaceColIfNullable: { time: '0' },
-//   })
-// })(csvDataChunks[increment])
-
+/**
+ * Порядок действий:
+ * 
+ * 1. Скачать из pgAdmin файл экспорта в .csv
+ * 2. Создать выше-задекларированные переменные в консоли
+ * 3. Скопировать содержимое файла экспорта
+ * 4. Запушить в переменную csvDataChunks `...` с содержимым файла
+ * 5. Скопировать скрипт и запустить в консоли браузера
+ */
 ;((csvData) => {
-  const tableColumns = ['code', 'time', 'text', 'avr_code', 'avr_text', 'wshd_order_id']
+  increment++
 
   return createSqlInsertQueryFromCsvExport({
     csvData,
     tableName: 'public.wshd_order_incidents',
-    tableColumns: tableColumns,
-    removeHeader: false,
+    tableColumns: ['code', 'time', 'text', 'avr_code', 'avr_text', 'orders_in_work_shift_data_id'],
+    removeHeader: true,
     skipRowIfNullableCols: { code: true },
     replaceColIfNullable: { time: '0' },
   })
-})(``)
+})(csvDataChunks[increment]);
+
+// ;((csvData) => {
+//   const tableColumns = ['code', 'time', 'text', 'avr_code', 'avr_text', 'wshd_order_id']
+
+//   return createSqlInsertQueryFromCsvExport({
+//     csvData,
+//     tableName: 'public.wshd_order_incidents',
+//     tableColumns: tableColumns,
+//     removeHeader: false,
+//     skipRowIfNullableCols: { code: true },
+//     replaceColIfNullable: { time: '0' },
+//   })
+// })(``)
